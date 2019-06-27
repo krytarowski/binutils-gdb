b13469beadaa6b01c0a8b824c044e6bbc25e2ca4
Kamil Rytarowski <n54@gmx.com>
2019-06-27 02:05:58 +0200

    fix

--- gdb/nat/netbsd-waitpid.h
+++ gdb/nat/netbsd-waitpid.h
@@ -1,4 +1,4 @@
-/* Wrapper for waitpid for GNU/Linux (LWP layer).
+/* Wrapper for waitpid for NetBSD (LWP layer).
 
    Copyright (C) 2000-2019 Free Software Foundation, Inc.
 
@@ -17,8 +17,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#ifndef NAT_LINUX_WAITPID_H
-#define NAT_LINUX_WAITPID_H
+#ifndef NAT_NETBSD_WAITPID_H
+#define NAT_NETBSD_WAITPID_H
 
 /* Wrapper function for waitpid which handles EINTR.  */
 extern int my_waitpid (int pid, int *status, int flags);
@@ -27,4 +27,4 @@ extern int my_waitpid (int pid, int *status, int flags);
    messages only.  */
 extern char *status_to_str (int status);
 
-#endif /* NAT_LINUX_WAITPID_H */
+#endif /* NAT_NETBSD_WAITPID_H */
