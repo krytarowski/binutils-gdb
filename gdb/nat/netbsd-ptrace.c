53f7dec0991ae9780090bd05162f99f904d57117
Kamil Rytarowski <n54@gmx.com>
2019-06-28 03:38:39 +0200

    fix

--- gdb/nat/netbsd-ptrace.c
+++ gdb/nat/netbsd-ptrace.c
@@ -56,48 +56,6 @@ kill_child (pid_t child, const char *who)
     }
 }
 
-/* Helper function to fork a process and make the child process call
-   the function FUNCTION, passing CHILD_STACK as parameter.
-
-   For MMU-less targets, clone is used instead of fork, and
-   CHILD_STACK is used as stack space for the cloned child.  If NULL,
-   stack space is allocated via malloc (and subsequently passed to
-   FUNCTION).  For MMU targets, CHILD_STACK is ignored.  */
-
-static int
-netbsd_fork_to_function (gdb_byte *child_stack, int (*function) (void *))
-{
-  int child_pid;
-
-  /* Sanity check the function pointer.  */
-  gdb_assert (function != NULL);
-
-#if defined(__UCLIBC__) && defined(HAS_NOMMU)
-#define STACK_SIZE 4096
-
-    if (child_stack == NULL)
-      child_stack = (gdb_byte *) xmalloc (STACK_SIZE * 4);
-
-    /* Use CLONE_VM instead of fork, to support uCnetbsd (no MMU).  */
-#ifdef __ia64__
-      child_pid = __clone2 (function, child_stack, STACK_SIZE,
-			    CLONE_VM | SIGCHLD, child_stack + STACK_SIZE * 2);
-#else /* !__ia64__ */
-      child_pid = clone (function, child_stack + STACK_SIZE,
-			 CLONE_VM | SIGCHLD, child_stack + STACK_SIZE * 2);
-#endif /* !__ia64__ */
-#else /* !defined(__UCLIBC) && defined(HAS_NOMMU) */
-  child_pid = fork ();
-
-  if (child_pid == 0)
-    function (NULL);
-#endif /* defined(__UCLIBC) && defined(HAS_NOMMU) */
-
-  if (child_pid == -1)
-    perror_with_name (("fork"));
-
-  return child_pid;
-}
 
 /* A helper function for netbsd_check_ptrace_features, called after
    the child forks a grandchild.  */
@@ -120,7 +78,7 @@ netbsd_grandchild_function (void *child_stack)
 static int
 netbsd_child_function (void *child_stack)
 {
-  ptrace (PTRACE_TRACEME, 0, (PTRACE_TYPE_ARG3) 0, (PTRACE_TYPE_ARG4) 0);
+  ptrace (PT_TRACE_ME, 0, NULL, 0);
   kill (getpid (), SIGSTOP);
 
   /* Fork a grandchild.  */
@@ -151,121 +109,4 @@ netbsd_enable_event_reporting (pid_t pid)
   event.pe_set_event |= PTRACE_POSIX_SPAWN;
 
   ptrace (PT_SET_EVENT_MASK, pid, &event, sizeof(event));
-}
-
-/* Disable reporting of all currently supported ptrace events.  */
-
-void
-netbsd_disable_event_reporting (pid_t pid)
-{
-  /* Set the options.  */
-  ptrace (PTRACE_SETOPTIONS, pid, (PTRACE_TYPE_ARG3) 0, 0);
-}
-
-/* Returns non-zero if PTRACE_OPTIONS is contained within
-   SUPPORTED_PTRACE_OPTIONS, therefore supported.  Returns 0
-   otherwise.  */
-
-static int
-ptrace_supports_feature (int ptrace_options)
-{
-  if (supported_ptrace_options == -1)
-    netbsd_check_ptrace_features ();
-
-  return ((supported_ptrace_options & ptrace_options) == ptrace_options);
-}
-
-/* Returns non-zero if PTRACE_EVENT_FORK is supported by ptrace,
-   0 otherwise.  Note that if PTRACE_EVENT_FORK is supported so is
-   PTRACE_EVENT_CLONE, PTRACE_EVENT_EXEC and PTRACE_EVENT_VFORK,
-   since they were all added to the kernel at the same time.  */
-
-int
-netbsd_supports_tracefork (void)
-{
-  return ptrace_supports_feature (PTRACE_O_TRACEFORK);
-}
-
-/* Returns non-zero if PTRACE_EVENT_EXEC is supported by ptrace,
-   0 otherwise.  Note that if PTRACE_EVENT_FORK is supported so is
-   PTRACE_EVENT_CLONE, PTRACE_EVENT_FORK and PTRACE_EVENT_VFORK,
-   since they were all added to the kernel at the same time.  */
-
-int
-netbsd_supports_traceexec (void)
-{
-  return ptrace_supports_feature (PTRACE_O_TRACEEXEC);
-}
-
-/* Returns non-zero if PTRACE_EVENT_CLONE is supported by ptrace,
-   0 otherwise.  Note that if PTRACE_EVENT_CLONE is supported so is
-   PTRACE_EVENT_FORK, PTRACE_EVENT_EXEC and PTRACE_EVENT_VFORK,
-   since they were all added to the kernel at the same time.  */
-
-int
-netbsd_supports_traceclone (void)
-{
-  return ptrace_supports_feature (PTRACE_O_TRACECLONE);
-}
-
-/* Returns non-zero if PTRACE_O_TRACEVFORKDONE is supported by
-   ptrace, 0 otherwise.  */
-
-int
-netbsd_supports_tracevforkdone (void)
-{
-  return ptrace_supports_feature (PTRACE_O_TRACEVFORKDONE);
-}
-
-/* Returns non-zero if PTRACE_O_TRACESYSGOOD is supported by ptrace,
-   0 otherwise.  */
-
-int
-netbsd_supports_tracesysgood (void)
-{
-  return ptrace_supports_feature (PTRACE_O_TRACESYSGOOD);
-}
-
-/* Display possible problems on this system.  Display them only once per GDB
-   execution.  */
-
-void
-netbsd_ptrace_init_warnings (void)
-{
-  static int warned = 0;
-
-  if (warned)
-    return;
-  warned = 1;
-
-  netbsd_ptrace_test_ret_to_nx ();
-}
-
-/* Extract extended ptrace event from wait status.  */
-
-int
-netbsd_ptrace_get_extended_event (int wstat)
-{
-  return (wstat >> 16);
-}
-
-/* Determine whether wait status denotes an extended event.  */
-
-int
-netbsd_is_extended_waitstatus (int wstat)
-{
-  return (netbsd_ptrace_get_extended_event (wstat) != 0);
-}
-
-/* Return true if the event in LP may be caused by breakpoint.  */
-
-int
-netbsd_wstatus_maybe_breakpoint (int wstat)
-{
-  return (WIFSTOPPED (wstat)
-	  && (WSTOPSIG (wstat) == SIGTRAP
-	      /* SIGILL and SIGSEGV are also treated as traps in case a
-		 breakpoint is inserted at the current PC.  */
-	      || WSTOPSIG (wstat) == SIGILL
-	      || WSTOPSIG (wstat) == SIGSEGV));
 }
